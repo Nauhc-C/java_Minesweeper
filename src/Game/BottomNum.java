2d2c84a6cf6665555263216fbff9aff1aaecee73
Lockbear <106546999+Lockbear@users.noreply.github.com>
2022-07-06 14:11:24 +0800

    Update BottomNum.java

--- src/Game/BottomNum.java
+++ src/Game/BottomNum.java
@@ -1,15 +1,16 @@
 package Game;
+
 //用于生成数字
 public class BottomNum {
-    {
-        for(int i = 1;i<=GameUtil.Map_W;i++){
-            for(int j = 1;j<=GameUtil.Map_H;j++){
-                //System.out.println(i+"    "+j);
-                if(GameUtil.DATA_BOTTOM[i][j]==-1){
-                    for(int k=i-1;k<=i+1;k++){
-                        for(int l=j-1;l<=j+1;l++){
-                            if(GameUtil.DATA_BOTTOM[k][l]>=0){
-                                GameUtil.DATA_BOTTOM[k][l] ++;
+    void newNum() {
+        for (int i = 1; i <= GameUtil.Map_W; i++) {
+            for (int j = 1; j <= GameUtil.Map_H; j++) {
+                // System.out.println(i+" "+j);
+                if (GameUtil.DATA_BOTTOM[i][j] == -1) {
+                    for (int k = i - 1; k <= i + 1; k++) {
+                        for (int l = j - 1; l <= j + 1; l++) {
+                            if (GameUtil.DATA_BOTTOM[k][l] >= 0) {
+                                GameUtil.DATA_BOTTOM[k][l]++;
                             }
                         }
                     }
@@ -18,3 +19,4 @@ public class BottomNum {
         }
     }
 }
+
