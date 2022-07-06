4c172764ae3f03c3e3f1625741137e18284410c5
Lockbear <106546999+Lockbear@users.noreply.github.com>
2022-07-06 13:58:42 +0800

    Update GameUtil.java

--- src/Game/GameUtil.java
+++ src/Game/GameUtil.java
@@ -1,69 +1,53 @@
-package com.sxt;
+package Game;
 
 import java.awt.*;
 
-/**
- * 工具类
- * 存放静态参数
- * 工具方法
- */
 public class GameUtil {
-    //地雷个数
-    static int RAY_MAX = 5;
-    //地图的宽
-    static int MAP_W = 11;
-    //地图的高
-    static int MAP_H = 11;
-    //雷区偏移量
-    static int OFFSET = 45;
-    //格子边长
-    static int SQUARE_LENGTH = 50;
+    static int Map_W = 31;// 地图宽
+    static int Map_H = 16;// 地图高
+    static int offset = 45;// 偏移量
+    static int Square_length = 50;// 格子边长
 
-    //插旗数量
+    static int RAY_MAX = 71;
+    // 游戏状态 0 表示游戏中 1 胜利 2 失败 3难度选择
+    static int state = 3;
+    // 游戏难度
+    static int level;
+    // 插旗数量
     static int FLAG_NUM = 0;
-
-    //鼠标相关
-    //坐标
+    // 鼠标相关参数
     static int MOUSE_X;
     static int MOUSE_Y;
-    //状态
     static boolean LEFT = false;
     static boolean RIGHT = false;
+    // 计时
+    static long Start_time;
+    static long End_time;
+    static long Play_time;
+    // 底层元素 -1 雷 0空 1-8代表数字
+    static int[][] DATA_BOTTOM = new int[Map_W + 2][Map_H + 2];
+    static int[][] DATA_TOP = new int[Map_W + 2][Map_H + 2]; // 顶层元素 -1 无覆盖 0 覆盖 1插旗 2插错旗
 
-    //游戏状态 0 表示游戏中 1 胜利 2 失败
-    static int state = 0;
-
-    //倒计时
-    static long START_TIME;
-    static long END_TIME;
-
-    //底层元素  -1 雷 0 空 1-8 表示对应数字
-    static int[][] DATA_BOTTOM = new int[MAP_W+2][MAP_H+2];
-    //顶层元素  -1 无覆盖 0 覆盖 1 插旗 2 差错旗
-    static int[][] DATA_TOP = new int[MAP_W+2][MAP_H+2];
-
-    //载入图片
-    static Image lei = Toolkit.getDefaultToolkit().getImage("imgs/lei.png");
-    static Image top = Toolkit.getDefaultToolkit().getImage("imgs/top.gif");
-    static Image flag = Toolkit.getDefaultToolkit().getImage("imgs/flag.gif");
-    static Image noflag = Toolkit.getDefaultToolkit().getImage("imgs/noflag.png");
-
-    static Image face = Toolkit.getDefaultToolkit().getImage("imgs/face.png");
-    static Image over = Toolkit.getDefaultToolkit().getImage("imgs/over.png");
-    static Image win = Toolkit.getDefaultToolkit().getImage("imgs/win.png");
-
+    // 载入图片
+    static Image bomb = Toolkit.getDefaultToolkit().getImage("image/bomb.png");
     static Image[] images = new Image[9];
     static {
-        for (int i = 1; i <=8 ; i++) {
-            images[i] = Toolkit.getDefaultToolkit().getImage("imgs/num/"+i+".png");
+        for (int i = 1; i <= 8; i++) {
+            images[i] = Toolkit.getDefaultToolkit().getImage("image/" + i + ".png");
         }
     }
+    static Image top = Toolkit.getDefaultToolkit().getImage("image/top.png");
+    static Image flag = Toolkit.getDefaultToolkit().getImage("image/flag.png");
+    static Image worseFlag = Toolkit.getDefaultToolkit().getImage("image/worseflag.png");
+    static Image gaming = Toolkit.getDefaultToolkit().getImage("image/gaming.png");
+    static Image over = Toolkit.getDefaultToolkit().getImage("image/over.png");
+    static Image win = Toolkit.getDefaultToolkit().getImage("image/win.png");
 
-    static void drawWord(Graphics g,String str,int x,int y,int size,Color color){
+    // 画字符（雷的个数以及开始和结束时间）
+    static void drawWord(Graphics g, String str, int x, int y, int size, Color color) {
         g.setColor(color);
-        g.setFont(new Font("仿宋",Font.BOLD,size));
-        g.drawString(str,x,y);
+        g.setFont(new Font("仿宋", Font.BOLD, size));
+        g.drawString(str, x, y);
 
     }
-
 }
