5a3bd7d638eacf7d5f6a9e35322269efd5511e11
Lockbear <106546999+Lockbear@users.noreply.github.com>
2022-07-06 11:44:42 +0800

    Update GameWin.java

--- src/Game/GameWin.java
+++ src/Game/GameWin.java
@@ -1,58 +1,94 @@
-package Game;
+package com.sxt;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class GameWin extends JFrame {
-    int width = 2*GameUtil.offset + GameUtil.Map_W*GameUtil.Square_length;
-    int height = 4*GameUtil.offset + GameUtil.Map_H*GameUtil.Square_length;
-    MapBotton mapBotton = new MapBotton();
-    MapTop mapTop = new MapTop();
+
+    int wigth = 2 * GameUtil.OFFSET + GameUtil.MAP_W * GameUtil.SQUARE_LENGTH;
+    int height = 4 * GameUtil.OFFSET + GameUtil.MAP_H * GameUtil.SQUARE_LENGTH;
 
     Image offScreenImage = null;
-    public void launch(){
+
+    MapBottom mapBottom = new MapBottom();
+    MapTop mapTop = new MapTop();
+
+    void launch(){
+        GameUtil.START_TIME=System.currentTimeMillis();
         this.setVisible(true);
-        this.setSize(width,height);
+        this.setSize(wigth,height);
         this.setLocationRelativeTo(null);
-        this.setTitle("川川的扫雷");
+        this.setTitle("尚学堂扫雷游戏");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //获取鼠标点击
+        //鼠标事件
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                if(e.getButton()==1){  //左键
+                switch (GameUtil.state){
+                    case 0 :
+                        if(e.getButton()==1){
+                        GameUtil.MOUSE_X = e.getX();
+                        GameUtil.MOUSE_Y = e.getY();
+                        GameUtil.LEFT = true;
+                        }
+                        if(e.getButton()==3) {
+                            GameUtil.MOUSE_X = e.getX();
+                            GameUtil.MOUSE_Y = e.getY();
+                            GameUtil.RIGHT = true;
+                        }
+                    case 1 :
+                    case 2 :
+                        if(e.getButton()==1){
+                            if(e.getX()>GameUtil.OFFSET + GameUtil.SQUARE_LENGTH*(GameUtil.MAP_W/2)
+                                    && e.getX()<GameUtil.OFFSET + GameUtil.SQUARE_LENGTH*(GameUtil.MAP_W/2) + GameUtil.SQUARE_LENGTH
+                                    && e.getY()>GameUtil.OFFSET
+                                    && e.getY()<GameUtil.OFFSET+GameUtil.SQUARE_LENGTH){
+                                mapBottom.reGame();
+                                mapTop.reGame();
+                                GameUtil.FLAG_NUM=0;
+                                GameUtil.START_TIME=System.currentTimeMillis();
+                                GameUtil.state=0;
+                            }
+                        }
+                        break;
+                    default:
 
-                    GameUtil.MOUSE_X = e.getX();
-                    GameUtil.MOUSE_Y = e.getY();
-                    GameUtil.LEFT =true;
-                    GameUtil.RIGHT = false;
-                    System.out.println("1"+GameUtil.LEFT);
                 }
-                if(e.getButton()==3){  //左键
-                    System.out.println("3");
-                    GameUtil.MOUSE_X = e.getX();
-                    GameUtil.MOUSE_Y = e.getY();
-                    GameUtil.LEFT =false;
-                    GameUtil.RIGHT = true;
-                }
+
+
             }
         });
-        while(true){
+
+
+        while (true){
             repaint();
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public void paint(Graphics g){
-        offScreenImage = this.createImage(width,height);
+    @Override
+    public void paint(Graphics g) {
+        offScreenImage = this.createImage(wigth,height);
         Graphics gImage = offScreenImage.getGraphics();
-        gImage.setColor(Color.pink);
-        gImage.fillRect(0,0,width,height);
-        mapBotton.paintSelf(gImage);
+        //设置背景颜色
+        gImage.setColor(Color.orange);
+        gImage.fillRect(0,0,wigth,height);
+
+        mapBottom.paintSelf(gImage);
         mapTop.paintSelf(gImage);
         g.drawImage(offScreenImage,0,0,null);
     }
 
-
+    public static void main(String[] args) {
+        GameWin gameWin = new GameWin();
+        gameWin.launch();
+    }
 }
+
